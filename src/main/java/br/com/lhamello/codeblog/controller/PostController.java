ad26e9dcc34ae2e0720fd23ce89b179a62d58455
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-05 13:24:12 -0300

    Criado arquivo para página de posts e método para salvar novo post.

--- src/main/java/br/com/lhamello/codeblog/controller/PostController.java
+++ src/main/java/br/com/lhamello/codeblog/controller/PostController.java
@@ -1,12 +1,17 @@
 package br.com.lhamello.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.lhamello.codeblog.model.Post;
 import br.com.lhamello.codeblog.service.PostService;
@@ -34,5 +39,22 @@ public class PostController {
     Post post = postService.findById(id);
     modelView.addObject("post", post);
     return modelView;
+  }
+
+  @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+  public String getPostForm() {
+    return "postForm";
+  }
+
+  @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+  public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+    if (result.hasErrors()) {
+      return "redirect:/newpost";
+    }
+
+    post.setData(LocalDate.now());
+    postService.save(post);
+
+    return "redirect:/posts";
   }
 }
